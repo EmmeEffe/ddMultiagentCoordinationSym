e513ee47281b9f634fa0792fd4c84df7a3ad73e0
EmmeEffe <martino.falorni@gmail.com>
2023-11-07 14:44:00 +0100

    Corrected launchfile

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,5 +1,6 @@
 
 # File from github.com/joshnewans/articubot_one
+# Edited by EmmeEffe to fit to ros Humble
 
 import os
 
@@ -31,11 +32,11 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
