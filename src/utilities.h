9ffef4f9732c8c5ccc055ad5b0db67eddf5ca412
EmmeEffe <martino.falorni@gmail.com>
2023-11-22 11:03:33 +0100

    calculate weights of graph

---
+++ src/utilities.h
@@ -0,0 +1,80 @@
+/*
+ * Library containing all the utilities functions
+*/
+
+#include<Eigen/Dense>
+#include<iostream>
+
+using Eigen::MatrixXf;
+using Eigen::MatrixXd;
+using Eigen::VectorXf;
+using Eigen::VectorXd;
+
+#define DEBUG
+
+
+/*
+ * Return if an element is in vector
+*/
+
+bool isInVector(VectorXd vect, int elem){
+    for(int i=0; i<vect.size(); i++){
+        if(vect(i)==elem)
+            return true;
+    }
+    return false;
+}
+
+/*
+ * Get the weight matrix (formula 1 of the paper)
+ *           /
+ *          | 0 when i=j or (j, i) not in E
+ * w_i_j = <  b_j when j in T (target list) and (j, i) in E
+ *          |  a_i_j when both i,j in F and (j, i) in E
+ *           \
+ * 
+ * B has dimension N, A and adjacency have dimension NxN, Targets has dimension M and Followers has dimension N-M
+*/
+
+MatrixXf getWeighMatrix(VectorXd Targets, VectorXd Followers, MatrixXd adjacency, VectorXf b_coeff, MatrixXf a_coeff){
+
+    // Verify dimension
+    int N = Targets.size() + Followers.size();
+    if(!(adjacency.rows()==adjacency.cols()&&adjacency.cols()==N)){
+        // TODO Raise an error
+    }
+    if(!(a_coeff.rows()==a_coeff.cols()&&a_coeff.cols()==N)){
+        // TODO Raise an error
+    }
+    if(!(b_coeff.size()==N)){
+        // TODO Raise an error
+    }
+
+    MatrixXf weigths(N, N);
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            if(i==j){                                           // If i==j then zero
+                weigths(i,j) = 0;
+                continue;
+            }
+            if(adjacency(j,i)==0){                              // If no edge (j,i) then zero
+                weigths(i,j) = 0;
+                continue;
+            }
+            if((isInVector(Targets, j))&&(adjacency(j,i==1))){  // If j Target and edge (j,i) then b_j
+                weigths(i,j) = b_coeff(j);
+                continue;
+            }
+            if((isInVector(Followers, i))&&(isInVector(Followers, j))&&(adjacency(j,i==1))){  // If i,j Followers and edge (j,i) then a_i_j
+                weigths(i,j) = a_coeff(i,j);
+            }
+        }
+    }
+
+    #ifdef DEBUG
+        std::cout<<"Weights Matrix: "<<weigths;
+    #endif
+
+    return weigths;
+}
