b12bf64bafeef5620a0eeecce64d79895bad7c51
EmmeEffe <martino.falorni@gmail.com>
2023-12-01 15:09:43 +0100

    Set control system to gain feedback loop

--- src/control_system_proportional.cpp
+++ src/control_system_proportional.cpp
@@ -9,6 +9,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
 #include "utilities.h"
 
 struct xyObject{
@@ -18,7 +19,9 @@ struct xyObject{
 
 class ControlSystemProportional : public rclcpp::Node {
 public:
-  ControlSystemProportional() : Node("control_system_proportional") {
+  ControlSystemProportional() : Node("control_system_proportional") {    
+    // Subscribe to Clock topic
+    clock_subscription = this->create_subscription<rosgraph_msgs::msg::Clock>("clock", rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&ControlSystemProportional::timeTick, this, std::placeholders::_1)); // Subscribe to clock data
 
     // Subscribe to the odometry topic
     odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
@@ -35,10 +38,12 @@ public:
     acc_publisher = this->create_publisher<std_msgs::msg::Float64MultiArray>("cmd_acc", 10); // Publish new point movement data
 
     // Declare parameters
-    this->declare_parameter("gain", 0.01); // Proportional gain
+    this->declare_parameter("vel_gain", 1); // Proportional gain
+    this->declare_parameter("acc_gain", 1); // Proportional gain
 
     // Get the parameter
-    this->get_parameter("gain", gain);
+    this->get_parameter("vel_gain", vel_gain);
+    this->get_parameter("acc_gain", acc_gain);
 
     oldsec = this->now().seconds(); // Initialize the time
     #ifdef DEBUG
@@ -47,34 +52,42 @@ public:
   }
 
 private:
+  void timeTick(const rosgraph_msgs::msg::Clock::SharedPtr msg) {
+    // Time ticked
+    double tau = (double)(msg->clock.nanosec - oldSec)/1000000.0; // Tau in seconds
+    oldSec = msg->clock.nanosec;
+
+    xyObject errorPos, desiredVel, errorVel;
+    errorPos.x = (targetPos.x - currentPos.x);
+    errorPos.y = (targetPos.y - currentPos.y);
+
+    desiredVel.x = vel_gain * errorPos.x;
+    desiredVel.y = vel_gain * errorPos.y;
+
+    errorVel.x = desiredVel.x - vel.x;
+    errorVel.y = desiredVel.y - vel.y;
+
+    double theta = atan2(errorPos.y, errorPos.x);
+
+    std_msgs::msg::Float64MultiArray accel;
+    accel.data.resize(2);
+    accel.data.at(0) = acc_gain * errorVel.x;
+    accel.data.at(1) = acc_gain * errorVel.y;
+    
+    // Publish the message
+    acc_publisher->publish(accel);
+  }
+
   void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
     // IMPORTANT!!! Velocities data (twist) are in veichle reference frame
     // IMPORTANT!!! pose.orientation is a quaternion
 
     //double tau = this->now().seconds() - oldsec; // Calculate time step (timestep may vary a bit)
-
-    xyObject errorPos;
-    errorPos.x = (targetPos.x - msg->pose.pose.position.x);
-    errorPos.y = (targetPos.y - msg->pose.pose.position.y);
-
-    double theta = atan2(errorPos.y, errorPos.x);
-
-    float sqThresh = 0.3;
-    float const_acc = 0.02;
-    std_msgs::msg::Float64MultiArray accel;
-    accel.data.resize(2);
-
-    if(errorPos.x*errorPos.x + errorPos.y*errorPos.y<sqThresh){
-      accel.data.at(0) = 0;
-      accel.data.at(1) = 0;
-    }else{
-      accel.data.at(0) = const_acc * cos(theta);
-      accel.data.at(1) = const_acc * sin(theta);
-    }
-
-
-    // Publish the message
-    acc_publisher->publish(accel);
+    currentPos.x = msg->pose.pose.position.x;
+    currentPos.y = msg->pose.pose.position.y;
+    
+    vel.x = msg->twist.twist.linear.x;
+    vel.y = msg->twist.twist.linear.y;
   }
 
   void targetCallback(const std_msgs::msg::Float64MultiArray msg){ // Update Target Position
@@ -82,13 +95,16 @@ private:
     targetPos.y = msg.data.at(1);
   }
 
+  rclcpp::Subscription<rosgraph_msgs::msg::Clock>::SharedPtr clock_subscription;
+  uint32_t oldSec = 0;
+
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
   rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr target_pos_subscription;
   rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr acc_publisher;
 
-  float gain; // Distance between p and p tilde
+  float vel_gain, acc_gain; 
   double oldsec;
-  xyObject targetPos, oldvel;
+  xyObject targetPos, vel, currentPos;
 };
 
 int main(int argc, char **argv) {
