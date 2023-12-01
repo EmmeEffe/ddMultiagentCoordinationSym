def784f1c1eb7df99c58597cf4803ebb4cf02aba
EmmeEffe <martino.falorni@gmail.com>
2023-12-01 14:52:30 +0100

    accel_to_cmd_vel syncronized

--- src/accel_to_cmd_vel.cpp
+++ src/accel_to_cmd_vel.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
 #include "utilities.h"
 
 #define DEBUG
@@ -9,6 +10,10 @@
 class AccelToCmdVel : public rclcpp::Node {
 public:
   AccelToCmdVel() : Node("accel_to_cmd_vel") {
+    // Subscribe to Clock topic
+    clock_subscription = this->create_subscription<rosgraph_msgs::msg::Clock>("clock", rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&AccelToCmdVel::timeTick, this, std::placeholders::_1)); // Subscribe to clock data
+
+
     // Subscribe to the odometry topic
     odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "newpt_coordinates", 10, std::bind(&AccelToCmdVel::odomCallback, this, std::placeholders::_1)
@@ -27,16 +32,36 @@ public:
     // Get the parameter
     this->get_parameter("dist_l", dist_l);
 
-    oldsec = this->now().seconds(); // Initialize the time
     #ifdef DEBUG
     //RCLCPP_INFO(this->get_logger, ("Init time in sec: " + oldsec).c_str());
     #endif
   }
 
 private:
+  void timeTick(const rosgraph_msgs::msg::Clock::SharedPtr msg) {
+      // Time ticked
+      double tau = (double)(msg->clock.nanosec - oldSec)/1000000.0; // Tau in seconds
+      oldSec = msg->clock.nanosec;
+
+      // Estimate velocity. It is calculate by integration between the current vel and the input acceleration, so it should not accumulate errors
+      double est_vel_x = linear_velocity_x + accel_x * tau;
+      double est_vel_y = linear_velocity_y + accel_y * tau;
+
+      // Remap Velocity of p tilde point to unicycle equivalent
+      Eigen::Vector2d unicycle_cmd = velocityToUnicycle(est_vel_x, est_vel_y, dist_l, theta_orientation);
+
+      // Create and publish the control command on cmd_vel topic
+      auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::Twist>();
+      cmd_vel_msg->linear.x = unicycle_cmd(0);
+      cmd_vel_msg->angular.z = unicycle_cmd(1); // TODO Control if is in radians
+
+      // Publish the command
+      cmd_vel_publisher_->publish(std::move(cmd_vel_msg));
+  }
+
   void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
     // Extract linear velocities from odometry message
-    // IMPORTANT!!! Velocities data (twist) are in vehicle reference frame
+    // IMPORTANT!!! Velocities data (twist) are usually in vehicle reference frame. In this case we don't read odom, but newpt_coordinates. That give us all the data in fixed frame
     linear_velocity_x = msg->twist.twist.linear.x;
     linear_velocity_y = msg->twist.twist.linear.y;
     theta_orientation = quaternionToTheta(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
@@ -45,44 +70,31 @@ private:
   void inputCallback(const std_msgs::msg::Float64MultiArray::SharedPtr msg){
     if (msg->data.size() == 2) {
       // Access the elements of the array
-      double accel_x = msg->data[0];
-      double accel_y = msg->data[1];
-
-      // Estimate velocity. It is calculate by integration between the current vel and the input acceleration, so it should not accumulate errors
-      double tau = this->now().seconds() - oldsec; // Calculate time step (timestep may vary a bit)
-      oldsec = this->now().seconds();
-      double est_vel_x = accel_x * tau;
-      double est_vel_y = accel_y * tau;
-
-      // Remap Velocity of p tilde point to unicycle equivalent
-      Eigen::Vector2d unicycle_oldVel = velocityToUnicycle(linear_velocity_x, linear_velocity_y, dist_l, theta_orientation);
-      Eigen::Vector2d unicycle_acc = velocityToUnicycle(est_vel_x, est_vel_y, dist_l, theta_orientation);
-      Eigen::Vector2d unicycle = decay_rate * unicycle_oldVel + unicycle_acc;
-
-      // Create and publish the control command on cmd_vel topic
-      auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::Twist>();
-      cmd_vel_msg->linear.x = unicycle(0);
-      cmd_vel_msg->angular.z = unicycle(1); // TODO Control if is in radians
-      cmd_vel_publisher_->publish(std::move(cmd_vel_msg));
-
+      accel_x = msg->data[0];
+      accel_y = msg->data[1];
     } else {
       RCLCPP_WARN(this->get_logger(), "Received array with incorrect size");
     }
   }
 
-  // Variables with parameters
+  // Variables from callbacks
+  // FROM Odom Callback
   double linear_velocity_x = 0;
   double linear_velocity_y = 0;
   double theta_orientation = 0;
-
-  float decay_rate = 0.6; 
+  //FROM Input Callback
+  double accel_x = 0;
+  double accel_y = 0;
 
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
   rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr input_subscription;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;  
+  
+  rclcpp::Subscription<rosgraph_msgs::msg::Clock>::SharedPtr clock_subscription;
+  uint32_t oldSec = 0;
 
+  // Parameters from file
   float dist_l; // Distance between p and p tilde
-  double oldsec;
 };
 
 int main(int argc, char **argv) {
